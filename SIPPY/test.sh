1d0644ec2f0f52b9bcc57b7b49059bb43f708d62
alquat <alqua@dtu.dk>
2024-12-17 16:13:56 +0100

    bash config added

--- SIPPY/test.sh
+++ SIPPY/test.sh
@@ -3,13 +3,16 @@
 # Set environment name and repository
 ENV_NAME="sys_id_module"
 
-# Step 1: Create a Conda environment
+
 echo "Creating Conda environment..."
 conda create -n $ENV_NAME python=3.9 -y
 
 # Step 2: Activate the environment
 echo "Activating the environment..."
 source activate $ENV_NAME
+conda install ipykernel -y
+python -m ipykernel install --user --name=$ENV_NAME
+conda install -c conda-forge slycot -y
 
 echo "Installing dependencies..."
 pip install --no-cache-dir -r requirements.txt
